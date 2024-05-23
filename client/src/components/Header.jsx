0cb08add75d9048ffbaf1fc2eda91dcc8d378118
Jagrit <jagritjain787@gmail.com>
2024-05-23 13:10:36 +0530

    complete dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,13 +10,16 @@ import {
 import React from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const { theme } = useSelector((state) => state.theme);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -42,19 +45,32 @@ export default function Header() {
       </Button>
 
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10- hidden sm:inline" color="gray" pill>
-          <FaMoon />
+        <Button
+          className="w-12 h-10- hidden sm:inline"
+          color="gray"
+          pill
+          onClick={() => {
+            dispatch(toggleTheme());
+          }}
+        >
+            {theme === "light"? (
+              <FaSun />
+            ) : (
+              <FaMoon />
+            )}
+          
         </Button>
-        
+
         {/*we check if the currentUser state exists then display user data,
         if not then only show sign-in! */}
         {currentUser ? (
           <Dropdown
             arrowIcon={false}
             inline
-            label=
-            {<Avatar alt="user" img={currentUser.profilePicture} rounded />}
-            >
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
             <Dropdown.Header>
               <span className="block text-sm">@{currentUser.username}</span>
               <span className="block text-sm font-medium truncate">
