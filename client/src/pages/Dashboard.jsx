84e91986ce7d8c56840e7ba027e2d6d00e9641d0
Jagrit <jagritjain787@gmail.com>
2024-05-26 12:41:30 +0530

    complete sidebar of dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
 
+import DashProfile from '../components/DashProfile'
+import DashSidebar from '../components/DashSidebar'
+import {useState, useEffect} from 'react'
+import {useLocation} from 'react-router-dom'
 export default function Dashboard() {
+    //TODO: To get what page we are at currently(the ?tab= somepage) this somepage we need to get
+  
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+//the second parameter in useEffect is and array of dependencies, the effect is re-run if change in any of the dependencies.
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
+      {/* posts...
+      {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {/* {tab === 'users' && <DashUsers />} */}
+      {/* comments  */}
+      {/* {tab === 'comments' && <DashComments />} */}
+      {/* dashboard comp */}
+      {/* {tab === 'dash' && <DashboardComp />} */}
+    </div>
+  );
 }
