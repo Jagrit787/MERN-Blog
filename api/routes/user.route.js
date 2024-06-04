4e4174b6acc61bf079962e16461b0cdf69a28ea1
Jagrit <jagritjain787@gmail.com>
2024-06-04 12:50:30 +0530

    add deleteUser api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { test, updateUser } from "../controllers/user.controller.js";
+import { test, updateUser, deleteUser} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 router.get("/test", test);
 //first the user will be verified by the verifyToken->IF VERIFIED THEN NEXT() FUNCTION IS CALLED which is updateUser()
 router.put("/update/:userId", verifyToken, updateUser);
 
+router.delete('/delete/:userId', verifyToken, deleteUser);
 export default router;
