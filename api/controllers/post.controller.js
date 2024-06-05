bc4b5267dd3c2220eb49f6a38cbe1634612934b7
Jagrit <jagritjain787@gmail.com>
2024-06-05 19:55:38 +0530

    create get posts api

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -25,3 +25,56 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPosts = async (req, res) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) ||0;
+    const limit = parseInt(req.query.limit) ||9;
+    const sortDirection= req.query.order === 'asc'? 1:-1;
+
+    //these are queries that can be used to access the blogs
+    //... operator is called spread
+
+
+    //creating a query to send to mongoDb based on the params passed by client in the query request
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        //to match either the title or content, "i" means 4search case-insensitively
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+  
