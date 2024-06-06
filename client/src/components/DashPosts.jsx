2bfb63c9ecde63886e19e6bbe8c0c9fbab9c1b32
Jagrit <jagritjain787@gmail.com>
2024-06-06 12:55:01 +0530

    add show more functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "flowbite-react";
+import { Button, Table } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true); 
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -15,6 +16,9 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if(data.posts.length <9) {
+              setShowMore(false)
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -24,6 +28,29 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+  const startIndex = userPosts.length;
+  console.log(startIndex)
+//TODO: to handle showMore function
+const handleShowMore = async()=>{
+  //start from the end of the list
+  const startIndex = userPosts.length;
+  console.log(startIndex)
+  try {
+    const res= await fetch(`/api/post/getposts?userId?=${currentUser._id}&startIndex=${startIndex}`);
+    const data= await res.json();
+    if(res.ok){
+      setUserPosts((prev)=> [...prev, ...data.posts]);
+
+      //if the next batch is less than 9 posts, disable show More
+      if(data.posts.length <9){
+        setShowMore(false)
+      }
+    }
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+  
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -81,6 +108,12 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          {showMore &&(
+            <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-am py-7"
+            >
+            Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>
