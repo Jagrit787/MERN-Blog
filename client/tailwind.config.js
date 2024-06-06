08f26959138c80491a23bc04fa8c974e0c1c11e0
Jagrit <jagritjain787@gmail.com>
2024-06-06 12:37:08 +0530

    show user posts inside dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 const flowbite = require("flowbite-react/tailwind");
+const scrollbar = require('tailwind-scrollbar')
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -11,5 +12,6 @@ export default {
   },
   plugins: [
     flowbite.plugin(),
+    scrollbar(),
   ],
 }
