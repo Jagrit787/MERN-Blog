b3789726784c795bcdc38f74ae6c9905f7b34973
Jagrit <jagritjain787@gmail.com>
2024-06-07 11:48:20 +0530

    create getUsers api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,15 +1,15 @@
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import User from '../models/user.model.js'
+import User from "../models/user.model.js";
 
 export const test = (req, res) => {
   res.json({ message: "API is working" });
 };
 
 export const updateUser = async (req, res, next) => {
-//     console.log("req.user.id: ", req.user.id);
-//   console.log("req.params.userId: ", req.params.userId);
-//   console.log("req.body: ", req.body);
+  //     console.log("req.user.id: ", req.user.id);
+  //   console.log("req.params.userId: ", req.params.userId);
+  //   console.log("req.body: ", req.body);
   //cookie id comes from->req.user.id
   //it needs to be checked with the userId supplied with update(frontend) ->req.params.userId
   if (req.user.id !== req.params.userId) {
@@ -60,25 +60,71 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
 export const deleteUser = async (req, res, next) => {
-  if(req.user.id !== req.params.userId){
-    return next(errorHandler(403, "You are not allowed to delete this user"))
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
   }
   try {
     await User.findByIdAndDelete(req.params.userId);
     res.status(200).json({ message: "User has been deleted" });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 //TODO: Signout
 export const signout = async (req, res, next) => {
   try {
-    res.clearCookie('access_token').status(200).json('User has been signed out')
+    res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User has been signed out");
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
+//TODO: GETuSER
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user)=>{
+      const { password,...rest } = user._doc;
+      return rest;
+    })
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date()
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    //$gte stands for greater than
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
