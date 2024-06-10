92ec68d6f7e71bb473589f20ed83a5b619f8a6ed
Jagrit <jagritjain787@gmail.com>
2024-06-10 14:02:03 +0530

    add edit functionality to comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
-import moment from 'moment';
-import {FaThumbsUp} from 'react-icons/fa'
+import moment from "moment";
+import { FaThumbsUp } from "react-icons/fa";
 import { useSelector } from "react-redux";
-export default function Comment({comment, onLike}) {
+import { Button, Textarea } from "flowbite-react";
+import { set } from "mongoose";
+export default function Comment({ comment, onLike, onEdit }) {
   const [user, setUser] = useState({});
-  const {currentUser}=useSelector(state=>state.user)
+  const [editedComment, setEditedComment] = useState(comment.content);
+  const [isEditing, setIsEditing] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -21,47 +25,121 @@ export default function Comment({comment, onLike}) {
     getUser();
   }, [comment]);
 
-//install moment to get the time of creation of the comment
-return (
-  <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
-    <div className='flex-shrink-0 mr-3'>
-      <img
-        className='w-10 h-10 rounded-full bg-gray-200'
-        src={user.profilePicture}
-        alt={user.username}
-      />
-    </div>
-    <div className='flex-1'>
-      <div className='flex items-center mb-1'>
-        <span className='font-bold mr-1 text-xs truncate'>
-          {user ? `@${user.username}` : 'anonymous user'}
-        </span>
-        
-        <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
-      </div>
-      <p className='text-gray-500 pb-2'>{comment.content}</p>
-      <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
-          <button
-            type='button'
-            onClick={() => onLike(comment._id)}
-            className={`text-gray-400 hover:text-blue-500 ${
-              currentUser &&
-              comment.likes.includes(currentUser._id) &&
-              '!text-blue-500'
-            }`}
-          >
-            <FaThumbsUp className='text-sm' />
-          </button>
+  //TODO: handle edit here because we only need to edit this comment
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditedComment(comment.content);
+  };
 
-          {/* to show the number of likes */}
-          <p className='text-gray-400'>
-            {comment.numberOfLikes > 0 &&
-              comment.numberOfLikes +
-                ' ' +
-                (comment.numberOfLikes === 1 ? 'like' : 'likes')}
-          </p>
+  //TODO: HANDLE SAVE COMMENT AFTER EDIT
+  const handleSave = async () => {
+    try {
+      const res = await fetch(`/api/comment/editComment/${comment._id}`,{
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          content: editedComment
+        })
+      })
+      if(res.ok) {
+        setIsEditing(false)
+        onEdit(comment, editedComment)
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //install moment to get the time of creation of the comment
+  return (
+    <div className="flex p-4 border-b dark:border-gray-600 text-sm">
+      <div className="flex-shrink-0 mr-3">
+        <img
+          className="w-10 h-10 rounded-full bg-gray-200"
+          src={user.profilePicture}
+          alt={user.username}
+        />
+      </div>
+      <div className="flex-1">
+        <div className="flex items-center mb-1">
+          <span className="font-bold mr-1 text-xs truncate">
+            {user ? `@${user.username}` : "anonymous user"}
+          </span>
+
+          <span className="text-gray-500 text-xs">
+            {moment(comment.createdAt).fromNow()}
+          </span>
         </div>
+
+        {/* //We wanna show only the textarea when editing */}
+        {isEditing ? (
+          <>
+            <Textarea
+              className="mb-2"
+              value={editedComment}
+              onChange={(e) => setEditedComment(e.target.value)}
+            />
+            <div className="flex justify-end gap-2 text-xs">
+              <Button
+                type="button"
+                size="sm"
+                gradientDuoTone="purpleToBlue"
+                onClick={handleSave}
+              >
+                Save
+              </Button>
+              <Button
+                type="button"
+                size="sm"
+                gradientDuoTone="purpleToBlue"
+                outline
+                onClick={() => setIsEditing(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="text-gray-500 pb-2">{comment.content}</p>
+            <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+              <button
+                type="button"
+                onClick={() => onLike(comment._id)}
+                className={`text-gray-400 hover:text-blue-500 ${
+                  currentUser &&
+                  comment.likes.includes(currentUser._id) &&
+                  "!text-blue-500"
+                }`}
+              >
+                <FaThumbsUp className="text-sm" />
+              </button>
+
+              {/* to show the number of likes */}
+              <p className="text-gray-400">
+                {comment.numberOfLikes > 0 &&
+                  comment.numberOfLikes +
+                    " " +
+                    (comment.numberOfLikes === 1 ? "like" : "likes")}
+              </p>
+
+              {/* We only show the edit button to the owner or the admin */}
+              {currentUser &&
+                (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                  <button
+                    type="button"
+                    className="text-gray-400 hover:text-blue-500"
+                    onClick={handleEdit}
+                  >
+                    Edit
+                  </button>
+                )}
+            </div>
+          </>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
 }
